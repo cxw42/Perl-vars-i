e0c923672a96dc2ba0b6e71261db2421f7d94b60
Chris White <chrisw@leehayes.com>
2019-04-26 12:33:41 -0400

    Initial Minilla commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::CXW::vars::i - It's new $module
+
+# SYNOPSIS
+
+    use Acme::CXW::vars::i;
+
+# DESCRIPTION
+
+Acme::CXW::vars::i is ...
+
+# LICENSE
+
+Copyright (C) Chris White.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Chris White <chrisw@leehayes.com>
---
+++ lib/Acme/CXW/vars/i.pm
@@ -0,0 +1,39 @@
+package Acme::CXW::vars::i;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::CXW::vars::i - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::CXW::vars::i;
+
+=head1 DESCRIPTION
+
+Acme::CXW::vars::i is ...
+
+=head1 LICENSE
+
+Copyright (C) Chris White.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Chris White E<lt>chrisw@leehayes.comE<gt>
+
+=cut
+
